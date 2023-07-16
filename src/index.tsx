2a322b5cbaa0afb038738211459d873d66658267
team7 <hnhyerim@gmail.com>
2023-07-16 23:29:16 +0900

    Feat: 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from 'pages/Home';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(<App />);
+const router = createBrowserRouter([
+  { path: '/', element: <App />, children: [{ index: true, path: '/', element: <Home /> }] },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<RouterProvider router={router} />);
