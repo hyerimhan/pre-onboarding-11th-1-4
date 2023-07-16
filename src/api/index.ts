f9f7acc64ba45844d16ef3b3ae3b42fa889267b8
team7 <hnhyerim@gmail.com>
2023-07-17 04:14:20 +0900

    Env: API 설정

---
+++ src/api/index.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export const GETSEARCH = async (params: string) => {
+  const response = await instance(`/sick?q=${params}`);
+  console.log(response);
+  return response;
+};
