83c9540a302db878561549e347feaecaa977740e
team7 <hnhyerim@gmail.com>
2023-07-18 20:02:39 +0900

    Feat: errorElement NotFound 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />, children: [{ index: true, path: '/', element: <Home /> }] },
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, path: '/', element: <Home /> }],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
